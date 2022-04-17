52a37e75a1d3961a16bd660937b43ece0b0074d3
Boopathi <Boopathi>
2022-04-17 18:57:49 +0530

    w

--- cypress/integration/arrayLength/sample1.js
+++ cypress/integration/arrayLength/sample1.js
@@ -3,3 +3,4 @@ console.log(" The elements of array are: " + arr);
 console.log(" <br>The length of the array is: " + arr.length);  
 //It is a simple example to understand how to calculate the length of an array using the array.length property.
 console.log(arr[1]);
+////////////////////////////
